12d0e85bacf0712efbb765ae6cf19ffd8e86984c
bilalabdulhadi <bilalabdulhadipro@gmail.com>
2023-01-27 20:51:01 +0300

    Matrix

--- 07-Matrix/09-Check Symmetric Matrix/main.c
+++ 07-Matrix/09-Check Symmetric Matrix/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int main ()
-{
+int main () {
     int A[100][100];
     int x=1;
     int R, C;
@@ -11,27 +10,27 @@ int main ()
     scanf ("%d", &C);
     puts ("\nIf Matrix[i][j] equals Matrix[j][i] so, this Matrix is a Symmetric matrix.\n");
     printf ("Enter the values of the matrix.>.>.>\n");
-    for (int i=0; i<R; i++){
-        for (int j=0; j<C; j++){
+    for (int i=0; i<R; i++) {
+        for (int j=0; j<C; j++) {
             printf ("Enter the values of Matrix[%d][%d]...", i+1, j+1);
             scanf ("%d", &A[i][j]);
         }
     }
-    for (int i=0; i<R; i++){
-        for (int j=0; j<C; j++){
+    for (int i=0; i<R; i++) {
+        for (int j=0; j<C; j++) {
             printf ("%6d", A[i][j]);
         }
         printf ("\n");
     }
-    for (int i=0; i<R; i++){
-        for (int j=0; j<C; j++){
-            if ( A[i][j] != A[j][i] ){
+    for (int i=0; i<R; i++) {
+        for (int j=0; j<C; j++) {
+            if ( A[i][j] != A[j][i] ) {
                 x=0;
                 break;
             }
         }
     }
-    if ( x==1 ){
+    if ( x==1 ) {
         printf ("This matrix is a Symmetric matrix.\n");
     }
     else {
