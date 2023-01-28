54eea81f4e3603a91aee06b0ecb3efdea19065ba
bilalabdulhadi <bilalabdulhadipro@gmail.com>
2023-01-28 22:46:49 +0300

    Matrix

--- 07-Matrix/09-Check Symmetric Matrix/main.c
+++ 07-Matrix/09-Check Symmetric Matrix/main.c
@@ -1,39 +1,41 @@
 #include <stdio.h>
 
-int main () {
-    int A[100][100];
-    int x=1;
-    int R, C;
-    printf ("Enter the number of rows of the matrix...");
-    scanf ("%d", &R);
-    printf ("Enter the number of columns of the matrix...");
-    scanf ("%d", &C);
-    puts ("\nIf Matrix[i][j] equals Matrix[j][i] so, this Matrix is a Symmetric matrix.\n");
-    printf ("Enter the values of the matrix.>.>.>\n");
-    for (int i=0; i<R; i++) {
-        for (int j=0; j<C; j++) {
-            printf ("Enter the values of Matrix[%d][%d]...", i+1, j+1);
-            scanf ("%d", &A[i][j]);
+/* This program to check a matrix entered by user if it symmetric or not. */
+
+int main() {
+    int row, column, flag = 1;
+    // Get the number of rows and columns of the matrix
+    printf("Enter the number of rows in the matrix: ");
+    scanf("%d", &row);
+    printf("Enter the number of columns in the matrix: ");
+    scanf("%d", &column);
+
+    // Declare the matrix
+    int matrix[row][column];
+
+    // Get the matrix elements
+    printf("Enter the matrix elements:\n");
+    for(int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            printf("matrix[%d][%d] : ", i+1, j+1);
+            scanf("%d", &matrix[i][j]);
         }
     }
-    for (int i=0; i<R; i++) {
-        for (int j=0; j<C; j++) {
-            printf ("%6d", A[i][j]);
-        }
-        printf ("\n");
-    }
-    for (int i=0; i<R; i++) {
-        for (int j=0; j<C; j++) {
-            if ( A[i][j] != A[j][i] ) {
-                x=0;
+
+    // Check if the matrix is symmetric or not
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < column; j++) {
+            if(matrix[i][j] != matrix[j][i]) {
+                flag = 0;
                 break;
             }
         }
     }
-    if ( x==1 ) {
-        printf ("This matrix is a Symmetric matrix.\n");
+
+    if(flag == 1) {
+        printf("\nThis matrix is a Symmetric matrix.\n");
     }
     else {
-        printf ("This matrix is not a Symmetric matrix.\n");
+        printf("\nThis matrix is not a Symmetric matrix.\n");
     }
 }
---
+++ 07-Matrix/10-Matrix Operations/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 1. Add Two Matrices
+ * 2. Subtract Two Matrices
+ * 3. Multiply Matrix By a Constant
+ * 4. Multiply Two Matrices
+ * 5. Power Of a Matrix
+ * 6. Determinant of a Matrix
+ * 7. Transpose of a Matrix
+ * 8. Check Symmetric Matrix
+*/
+
+void menu();
+void sum();
+
+int main() {
+    int option;
+
+    menu();
+    scanf("%d", &option);
+    if(option == 1) {
+
+    }
+    else
+        exit(0);
+    return 0;
+}
+
+void menu() {
+    printf(">>> Main Menu:\n");
+    printf(" 1. Add Two Matrices\n");
+    printf(" 2. Subtract Two Matrices\n");
+    printf(" 3. Multiply Matrix By a Constant\n");
+    printf(" 4. Multiply Two Matrices\n");
+    printf(" 5. Power Of a Matrix\n");
+    printf(" 6. Determinant of a Matrix\n");
+    printf(" 7. Transpose of a Matrix\n");
+    printf(" 8. Check Symmetric Matrix\n");
+    printf(" 9. Exit.\n>> Your option: ");
+}
+
+void sum() {
+
+}
