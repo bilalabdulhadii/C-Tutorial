680a9f5e0fabd2e44f5d34f207424b54482ba83c
bilalabdulhadi <bilalabdulhadipro@gmail.com>
2023-02-12 20:56:37 +0300

    Pointers

---
+++ 08-Pointers/01-Initialize and Use Pointers/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int *number_ptr;
+    int number = 10;
+
+    number_ptr = &number;
+
+    printf("Address of number : %d\n", &number);
+    printf("Address of number ptr : %d\n", &number_ptr);
+
+    printf("\n----------------------------------\n");
+    printf("Value of number : %d\n", number);
+    printf("Value of number ptr : %d\n", number_ptr);
+
+    printf("\n----------------------------------\n");
+
+    printf("Value pointed by number ptr = %d\n", *number_ptr);
+    return 0;
+}
